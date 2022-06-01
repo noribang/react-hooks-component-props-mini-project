1cd809d66293c7dee9a058e1067d9ef8688896ee
hoonkim <hoonk9387@gmail.com>
2022-05-31 22:27:08 -0500

    Refactored About component to use destructuring. Added default value for img src attribute.

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,27 @@
 import React from "react";
 
-function About(props) { 
+// function About(props) { 
+//     const alt = "blog logo";
+
+//     return (
+//         <aside>
+//             <img src={props.image} alt={alt}></img>
+//             <p>{props.about}</p>
+//         </aside>
+//     )
+// }
+
+function About({image = "https://via.placeholder.com/215", 
+                about}) { 
     const alt = "blog logo";
 
     return (
         <aside>
-            <img src={props.image} alt={alt}></img>
-            <p>{props.about}</p>
+            <img src={image} alt={alt}></img>
+            <p>{about}</p>
         </aside>
     )
 }
+
 
 export default About;
